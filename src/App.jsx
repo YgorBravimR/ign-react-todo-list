2cf0ba0bcd7145df2ce53b66770bde409711bb48
Ygor Bravim <ygorbravimr@gmail.com>
2022-09-02 15:20:06 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import styles from "./App.module.css";
 import { Header } from "./components/Header";
 import { Tasklist } from "./components/Tasklist";
-import { Tasks } from "./components/Tasks";
 import "./global.css";
 
 export function App() {
